9b949769ac8c5b214c03031793217ca093a86de0
Anthony2106 <an.perkins@duocuc.cl>
2023-10-12 13:49:38 -0300

    cambios contraseña autentificacion

--- src/autenticacion/Schema/autenticacion.schema.ts
+++ src/autenticacion/Schema/autenticacion.schema.ts
@@ -2,6 +2,6 @@ import { Schema } from "mongoose";
 
 export const AutenticacionSchema = new Schema({
   rut: { type: String, required: true },
-  contrasena: { type: String, required: true },
+  contrasena:{ type: String, required: true, bcrypt: true},
   usuario_id: { type: Schema.ObjectId, ref: "Usuario" },
 });
--- src/autenticacion/autenticacion.module.ts
+++ src/autenticacion/autenticacion.module.ts
@@ -2,10 +2,9 @@ import { Module } from "@nestjs/common";
 import { AutenticacionService } from "./autenticacion.service";
 import { AutenticacionController } from "./autenticacion.controller";
 import { MongooseModule } from "@nestjs/mongoose";
-import { AutenticacionSchema } from "./entities/autenticacion.schema";
 import { JwtService, JwtModule } from "@nestjs/jwt";
 import { jwtConstants } from "./constants";
-
+import { AutenticacionSchema } from "./Schema/autenticacion.schema";
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: "Autenticacion", schema: AutenticacionSchema }]),
